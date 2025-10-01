9db75cbb152fd76c9df8196e5765ab86fd9b6590
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-01 18:03:28 -0300

    feat: organizacao, refatoracao, identacao e formatacao

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:meal_museum/widgets/category_grid_item.dart';
 import 'package:meal_museum/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -8,9 +9,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Categories'),
-      ),
+      appBar: AppBar(title: const Text('Categories')),
       body: GridView(
         padding: const EdgeInsets.all(16),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -22,34 +21,8 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // grid de 2 colunas
           for (final category in availableCategories)
-            _buildCategoryItem(category.title, category.color),
+            CategoryGridItem(category: category),
         ],
-      ),
-    );
-  }
-
-  Widget _buildCategoryItem(String title, Color color) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Center(
-        child: Text(
-          title,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
       ),
     );
   }
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:meal_museum/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [category.color.withOpacity(0.7), category.color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Text(
+          category.title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
