c8c8aea5a63e8684d150c947f37ccb1d30995a43
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-01 19:49:59 -0300

    feat: exibicao dinamica de categorias e suas comidas

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:meal_museum/widgets/category_grid_item.dart';
 import 'package:meal_museum/data/dummy_data.dart';
 import 'package:meal_museum/screens/meals.dart';
+import 'package:meal_museum/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(titulo: 'Titulo', meals: []),
+        builder: (ctx) =>
+            MealsScreen(titulo: category.title, meals: filteredMeals),
       ),
     );
   }
@@ -32,7 +38,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: () => _selectCategory(context),
+              onSelectCategory: () => _selectCategory(context, category),
             ),
         ],
       ),
